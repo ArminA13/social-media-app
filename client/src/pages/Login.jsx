441d57435b2fac2d47302981a9ca9bcf2c025397
ArmineMuradyan <muradyan.armin13@gmail.com>
2024-02-13 00:48:10 +0400

    add initial commit for social media app

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-6'>
+      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
