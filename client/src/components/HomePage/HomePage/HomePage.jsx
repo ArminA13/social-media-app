5181d2855fa605f10795839dd11419b7b099a8e5
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2024-02-17 08:54:59 -0800

    Add dark-mode

--- client/src/components/HomePage/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage/HomePage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Nav from "../Nav/Nav";
-import './HomePage.css'
+import "./HomePage.css";
 import Main from "../Main/Main";
-
-
+import { useState } from "react";
 
 function HomePage() {
-  return <div>
-     <Nav />
-    <Main />
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className={isDarkMode ? "dark-mode" : ""}>
+      <Nav isDarkMode={isDarkMode} setDarkMode={setDarkMode} />
+      <Main />
     </div>
+  );
 }
 
 export default HomePage;
